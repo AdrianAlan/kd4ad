c03c16450523d75e9df793467cc571935707ce5a
Adrian Alan Pol <adrianalan.pol@gmail.com>
2023-03-10 09:01:59 +0200

    Add noise exposure experiment

---
+++ experiment-noise-exposure.py
@@ -0,0 +1,206 @@
+import torch
+
+from dataset import generate
+from evaluate import evaluation
+from models import (
+    Teacher,
+    StudentA,
+    StudentB,
+    StudentC,
+    StudentD,
+    StudentE,
+    StudentF,
+    StudentG,
+)
+from plotting import plot_loss
+
+from torch import nn
+from tqdm import tqdm
+
+
+def epoch(
+    teacher,
+    students,
+    device,
+    dataloader,
+    loss_fn_teacher,
+    loss_fn_student,
+    optimizer=None,
+):
+    total_loss = torch.tensor(0.0, dtype=torch.float).to(device)
+    scaler = torch.cuda.amp.GradScaler()
+    score = torch.nn.MSELoss(reduction="none")
+
+    for student in students:
+        student["total_loss"] = torch.tensor(0.0, dtype=torch.float).to(device)
+        if optimizer:
+            student["model"].train()
+        else:
+            student["model"].eval()
+
+    for image_batch in dataloader:
+        # Move tensor to the proper device
+        target = image_batch.to(device)
+        target_noise = target + 0.1 * torch.normal(0.0, 1.0, target.shape).to(device)
+        target_noise = torch.clip(target_noise, 0.0, 1.0)
+
+        if optimizer:
+            teacher.train()
+        else:
+            teacher.eval()
+
+        with torch.autocast(device_type="cuda", dtype=torch.float16):
+            decoded_data = teacher(target)
+            # Evaluate loss
+            loss = loss_fn_teacher(decoded_data, target)
+        # Backward pass
+        if optimizer:
+            optimizer.zero_grad()
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+        # Print batch loss
+        total_loss += loss.item()
+
+        teacher.eval()
+        for student in students:
+            output_score = student["model"](target_noise)
+            with torch.no_grad():
+                decoded_data = teacher(target_noise)
+                student_target = score(decoded_data, target_noise)
+                student_target = torch.log(torch.mean(student_target, (2, 3)) + 1)
+            loss = loss_fn_student(output_score, student_target)
+            if optimizer:
+                student["optimizer"].zero_grad()
+                loss.backward()
+                student["optimizer"].step()
+            # Print batch loss
+            student["total_loss"] += loss.item()
+
+    return total_loss.detach().cpu().numpy()
+
+
+def run_experiment(dataset, digit, epochs, batch_size, save_path):
+    # Define the loss function
+    loss_fn_teacher = torch.nn.MSELoss()
+    loss_fn_student = torch.nn.L1Loss()
+
+    # Set the random seed for reproducible results
+    torch.manual_seed(0)
+
+    # Check if the GPU is available
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    def init_weights(m):
+        if isinstance(m, nn.Linear):
+            torch.nn.init.kaiming_uniform_(m.weight)
+            m.bias.data.fill_(0.01)
+
+    # Initialize the teacher
+    teacher = Teacher().to(device).apply(init_weights)
+
+    # Define an optimizer
+    lr = 1e-3
+    optim_teacher = torch.optim.Adam(teacher.parameters(), lr=lr, weight_decay=1e-05)
+
+    # Get the dataset
+    train_loader, val_loader, test_loader, y_true = generate(digit, batch_size, dataset)
+
+    students = []
+    s_losses = []
+    for student in [
+        StudentA(),
+        StudentB(),
+        StudentC(),
+        StudentD(),
+        StudentE(),
+        StudentF(),
+        StudentG(),
+    ]:
+        student.to(device)
+        student.apply(init_weights)
+        optim_student = torch.optim.Adam(
+            student.parameters(), lr=lr, weight_decay=1e-05
+        )
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optim_student, "min")
+        model = {}
+        model["model"] = student
+        model["optimizer"] = optim_student
+        model["scheduler"] = scheduler
+        model["best_loss"] = float("inf")
+        students.append(model)
+        s_losses.append({"train": [], "val": []})
+
+    loss = {"train": [], "val": []}
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optim_teacher, "min")
+    best_loss = float("inf")
+    for _ in tqdm(range(epochs), desc="Progress"):
+        # Train
+        train_loss = epoch(
+            teacher,
+            students,
+            device,
+            train_loader,
+            loss_fn_teacher,
+            loss_fn_student,
+            optim_teacher,
+        ) / (len(train_loader) * batch_size)
+        for x, student in enumerate(students):
+            s_train_loss = student["total_loss"].detach().cpu().numpy() / (
+                len(val_loader) * batch_size
+            )
+            s_losses[x]["train"].append(s_train_loss)
+
+        # Evaluation
+        with torch.no_grad():
+            val_loss = epoch(
+                teacher, students, device, val_loader, loss_fn_teacher, loss_fn_student
+            ) / (len(val_loader) * batch_size)
+            if val_loss < best_loss:
+                torch.save(
+                    teacher.state_dict(),
+                    "{}/{}-{}-Teacher".format(save_path, dataset, digit),
+                )
+            scheduler.step(val_loss)
+            for x, student in enumerate(students):
+                s_val_loss = student["total_loss"].detach().cpu().numpy() / (
+                    len(val_loader) * batch_size
+                )
+                s_losses[x]["val"].append(s_val_loss)
+                if s_val_loss < student["best_loss"]:
+                    torch.save(
+                        student["model"].state_dict(),
+                        "{}/{}-{}-Student{}".format(
+                            save_path, dataset, digit, chr(65 + x)
+                        ),
+                    )
+
+                student["scheduler"].step(s_val_loss)
+        loss["train"].append(train_loss)
+        loss["val"].append(val_loss)
+
+    for x, loss_s in enumerate(s_losses):
+        plot_loss(
+            loss_s,
+            "{}/{}-{}-Student{}-Loss".format(save_path, dataset, digit, chr(65 + x)),
+        )
+    plot_loss(loss, "{}/{}-{}-Teacher-Loss".format(save_path, dataset, digit))
+
+    # Evaluation
+    evaluation(device, test_loader, y_true, dataset, digit, save_path)
+
+
+def main():
+    # Perform experiments for both datasetets and all digits
+    epochs = 300
+    batch_size = 100
+    save_path = "./results-noise"
+    for dataset in ["MNIST", "FMNIST"]:
+        for digit in range(10):
+            torch.cuda.empty_cache()
+            print("Testing digit {}".format(digit))
+            run_experiment(dataset, digit, epochs, batch_size, save_path)
+
+
+if __name__ == "__main__":
+    main()
